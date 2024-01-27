0d8d124ef62f40d42bb77d24b770cbf99d529a3e
Blaze <32179683+leon-fenzl@users.noreply.github.com>
2024-01-27 11:13:57 -0300

    Update 00

---
+++ Scripts/2DPlayerTD.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@export var speed = 300.0
+@export var zoom := Vector2.ZERO
+@onready var walk := Vector2.ZERO
+@onready var spr := $SprPlayer
+@onready var anim := $SprAnimation
+
+func _ready():
+	$Camera2D.zoom = zoom
+func _physics_process(delta):
+	Walk()
+	velocity = lerp(velocity,walk*speed*delta,10*delta)
+	move_and_slide()
+func Walk():
+	walk = Input.get_vector("left","right","forward","back").normalized()
+	
